3e62b3a3dc858aec98acea82152ee7703edc4788
Njoki <kinuthiash@gmail.com>
2022-04-27 00:17:35 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// Code your solution herey aybatch in the arr
+// holds sum of batter
+const totalBatteries = batteryBatches.reduce(function(acc ,element){
+  return acc += element
+} , 0)
+console.log(totalBatteries)
+
+
